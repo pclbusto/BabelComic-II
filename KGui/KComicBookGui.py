10008801fa692cd7da1495a1f324dc4d747be9a0
pclbusto <pclbusto@gmail.com>
2018-02-16 18:02:29 -0300

    correccion en comicBook Gui y vaolume

--- KGui/KComicBookGui.py
+++ KGui/KComicBookGui.py
@@ -4,7 +4,7 @@ from Entidades.ComicBooks import ComicBook
 from kivy.app import App
 from kivy.uix.screenmanager import Screen,ScreenManager
 from kivy.uix.image import Image
-from kivy.uix.widget import Widget
+from kivy.uix.screenmanager import SlideTransition
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.carousel import Carousel
@@ -15,7 +15,6 @@ class Comic(Screen):
 
     def __init__(self, comic=None, **kwargs):
         super(Comic, self).__init__(**kwargs)
-
         self.layout = FloatLayout()
         self.image_cover = Image(allow_stretch=True)
         #self.loadImageCover('Sin Caratula.jpg')
@@ -57,44 +56,79 @@ class Comic(Screen):
     def loadComic(self):
         self.loadImageCover(str(self.comic.comicId) + '.jpg')
         self.labelDescripcion.text = self.comic.resumen
+        self.name=str(self.comic.comicId)
 
-class ComicManager(Carousel):
+class ComicManager(ScreenManager):
     CANTIDAD_SLIDES = 2
     def __init__(self, session=None, **kwargs):
+
         super(ComicManager, self).__init__(**kwargs)
         if session is not None:
             self.session = session
         else:
             self.session = Entidades.Init.Session()
         self.comics = self.session.query(ComicBook.ComicBook).order_by(ComicBook.ComicBook.path.asc()).limit(ComicManager.CANTIDAD_SLIDES*2).all()
+        self.indice = 0
+        self.more_left = True
+        self.more_right = True
+        self.comic_screens =[]
         for comic in self.comics:
-            self.add_widget(Comic(comic))
+            self.comic_screens.append(Comic(comic))
         print(self.comics)
-    def on_current_slide(self, *args):
-        print(self.index)
-        print(self.slides[self.index].comic.comicId)
-        if self.index == (ComicManager.CANTIDAD_SLIDES*2)-1:
-            self.clear_widgets()
-            self.comics.clear()
+        self.switch_to(self.comic_screens[self.indice])
 
-            print(self.index)
-            print(self.comics)
-            comic_path = self.comics[self.index].path
-            print(comic_path)
-            for i in range(int(ComicManager.CANTIDAD_SLIDES)):
-                self.remove_widget(self.slides[0])
-                self.comics.pop(0)
-            self.comics.extend(self.session.query(ComicBook.ComicBook).order_by(ComicBook.ComicBook.path.asc()).\
-               filter(ComicBook.ComicBook.path > comic_path).limit(int(ComicManager.CANTIDAD_SLIDES)).all())
-            for comic in self.comics:
-               self.add_widget(Comic(comic))
-            self.index=CANTIDAD_SLIDES-1
-            print(self.index)
-            print(self.comics)
-
-
-
-
+    def on_touch_up(self, args):
+        print(self.indice)
+        print("COMICS {}".format(self.screens))
+        sl = SlideTransition()
+        if args.dpos[0] < 0:
+            if self.indice == (2 * ComicManager.CANTIDAD_SLIDES) - 1 or self.indice == len(self.comics) - 1:
+                print("AGREGANDO NUEVOS CMICS")
+                print(self.comics)
+                comic_path = self.comics[self.indice].path
+                lista = self.session.query(ComicBook.ComicBook).order_by(ComicBook.ComicBook.path.asc()). \
+                    filter(ComicBook.ComicBook.path >= comic_path).limit(int(2*ComicManager.CANTIDAD_SLIDES)).all()
+                if len(lista) > 1:
+                    print("COMICS {}".format(self.screens))
+                    self.comics.clear()
+                    self.comic_screens.clear()
+                    print("****LISTAS******")
+                    self.comics = lista
+                    print(self.comics)
+                    for comic in self.comics:
+                        self.comic_screens.append(Comic(comic))
+                    self.indice = 1
+                else:
+                    self.more_right = False
+            if self.more_right:
+                print("POR ACAAAAAAAAAAAAAAAA")
+                sl.direction = 'left'
+                self.transition = sl
+                self.indice += 1
+                self.switch_to(self.comic_screens[self.indice], direction='left')
+        if args.dpos[0] > 0:
+            if self.indice == 0:
+                print("AGREGANDO NUEVOS CMICS")
+                print(self.comics)
+                comic_path = self.comics[self.indice].path
+                lista = self.session.query(ComicBook.ComicBook).order_by(ComicBook.ComicBook.path.asc()). \
+                    filter(ComicBook.ComicBook.path <= comic_path).limit(int(2*ComicManager.CANTIDAD_SLIDES)).all()
+                if len(lista) > 1:
+                    print("COMICS {}".format(self.screens))
+                    self.comics.clear()
+                    self.comic_screens.clear()
+                    print("****LISTAS******")
+                    self.comics = lista
+                    print(self.comics)
+                    for comic in self.comics.reverse():
+                        self.comic_screens.append(Comic(comic))
+                    self.indice = 3
+                else:
+                    self.more_left = False
+            if self.more_left:
+                self.transition = sl
+                self.indice -= 1
+                self.switch_to(self.comic_screens[self.indice], direction='right')
 class KComicBookGuiapp(App):
     def build(self):
         return ComicManager()
