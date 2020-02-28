a78e667198531fd33d0e67d681624354552a923a
pedro <pclbusto@gmail.com>
2020-02-28 07:38:49 +0000

    test filtrado por paneles de editorial y volumen

--- Gui_gtk/test_managers.py
+++ Gui_gtk/test_managers.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
     session = Entidades.Init.Session()
     sq = session.query(Comicbook).join(Comicbook_Info, Comicbook_Info.id_comicbook_info == Comicbook.id_comicbook_info).\
         join(Volume, Comicbook_Info.id_volume == Volume.id_volume).join(Publisher, Volume.id_publisher == Publisher.id_publisher).\
-        filter(Publisher.id_publisher == '31')
+        filter(Publisher.id_publisher == '32')
 
     for comic in sq.all():
         print(comic)
